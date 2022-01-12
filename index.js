367f8e8028b23287cdd78ae178066f7a33f7e4f2
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-13 02:44:45 +0600

    Added post model with few routes

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://memoriesapp:memoriesapp@cluster0.nfwgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL)
+  .then(
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  )
+  .catch((error) => {
+    console.log(error.message);
+  });
