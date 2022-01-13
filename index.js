7587c1305ab86d4236a69cd35e71b295a420c517
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-14 02:32:38 +0600

    Added update routes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 
 app.use("/posts", postRoutes);
 
