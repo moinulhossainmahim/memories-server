70d85e1dc45847b8adb4dc005575dcaa9aa1daed
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-14 21:08:54 +0600

    Added env file

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://memoriesapp:memoriesapp@cluster0.nfwgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
   )
