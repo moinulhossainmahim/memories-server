eb72f2c91515a9a1170e8af92ff055bafd858fdd
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-14 16:17:31 +0600

    changed updated route functionality in controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,14 +22,18 @@ export const createPosts = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params;
   const post = req.body;
 
-  if (mongoose.Types.ObjectId.isValid(_id)) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send("No post with that id");
   }
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { ...post, id },
+    {
+      new: true,
+    }
+  );
   res.json(updatedPost);
 };
