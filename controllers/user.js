2bf8ef74be3b533730bfa349b458775f5b2cae15
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-20 02:07:46 +0600

    added user model

---
+++ controllers/user.js
@@ -0,0 +1,7 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const signin = async (req, res) => {};
+
+export const signup = async (req, res) => {};
