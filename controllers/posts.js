9dc0203b8b5f00459de889f5b874fc451341dcd2
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-24 02:57:48 +0600

    added search posts controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,6 +10,19 @@ export const getPosts = async (req, res) => {
   }
 };
 
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+  try {
+    const title = new RegExp(searchQuery, "i");
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const createPosts = async (req, res) => {
   const post = req.body;
   const newPost = new PostMessage({
