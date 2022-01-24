4935cae5f54b3603413ee63161fd9002ae320765
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-25 01:01:23 +0600

    created getPost route

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,6 +22,16 @@ export const getPosts = async (req, res) => {
   }
 };
 
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await PostMessage.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+};
+
 export const getPostsBySearch = async (req, res) => {
   const { searchQuery, tags } = req.query;
   try {
